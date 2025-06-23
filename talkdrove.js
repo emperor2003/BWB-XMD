04287abb02decc3a41ffe749027652d82c86be9e
emperor2003 <emperorcarson18@gmail.com>
2025-06-23 14:17:35 +0300

    .

--- talkdrove.js
+++ talkdrove.js
@@ -10,7 +10,7 @@
     "env": {
       "SESSION_ID": {
         "description": "Put your SESSION_ID here. Make sure it starts with ey~  or get session id from here,  https://charle-ke.onrender.com/",
-        "value":"",
+        "value":"eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRUVXMUpzY2pZajlDcnZ4MnIrK2puOG9Nd2pheERoRmY1WG1haldOOWFuMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib2ZPVjNBcFVEMWFpMysvVWxzU3hRZ1RQSFZVRmYydnVMekdlTG1aYjlTVT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI2QkFwSWhUdUcvMzZYbEUrR1F3bGxoNEhhanBaTW1abXFYd0hGU2loQldBPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ2THhVVm5KMmh3RTg2S3lxZnNiTFo5SkdQdk9tQ1E2cjVPRzVOa3QxM3pVPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IldON2FQUnRkeStDUDlBZEhWZ2h6ME5NQ2x0b1ppSEcxcG5IL3hGU3lXR2s9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlFZOEsyYk5CVjVCb1VVODhBbkVxS2ViU2xvaHY2d3ZhcDZ5U1ZKQ2RSeVU9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMkJzc2VvVlBKREtnMWFGcElyalhjSVpGYkx5bWZVR2UrZ3pQc2x1K0xHQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTjlRRnM5RHJqMlRDdmk3UTFLamhzN1hUNmQyd0g0ZnQ0OEFVc0F1NjRuQT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InpIS3piZWdzWjFMNU1KRVlCQ1ZXcHFRamxZdGRsVzlaZ2tERmFHdVJJeXU3dE11QU5QZU9NODJQRExOVDFwbTVHV3p0RXVZQldwR1ZCdjlVSUllRUFBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTk4LCJhZHZTZWNyZXRLZXkiOiJjSVcwUWxkN0h6dGRNNFlMUGJVQlpFNVNmWWlFa29ES2N3aUJCNEU5Q01rPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W3sia2V5Ijp7InJlbW90ZUppZCI6IjI1NDc0ODk3MzAwM0BzLndoYXRzYXBwLm5ldCIsImZyb21NZSI6dHJ1ZSwiaWQiOiI0MDREMTI2Qjk4MENDREI1MzQ0QUY5QTBFQjMyQTk3QyJ9LCJtZXNzYWdlVGltZXN0YW1wIjoxNzUwNjc2NzQ2fSx7ImtleSI6eyJyZW1vdGVKaWQiOiIyNTQ3NDg5NzMwMDNAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiREEzMDU0NUI1NTlGMjk1MDE4OEVGQ0IwREZGMjQzNjQifSwibWVzc2FnZVRpbWVzdGFtcCI6MTc1MDY3Njc1M31dLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTeW5jQ291bnRlciI6MSwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sInJlZ2lzdGVyZWQiOnRydWUsInBhaXJpbmdDb2RlIjoiOEg2WDJIM0giLCJtZSI6eyJpZCI6IjI1NDc0ODk3MzAwMzoxQHMud2hhdHNhcHAubmV0IiwibmFtZSI6IkxpbiIsImxpZCI6IjIyMTAyODI2MTMxNDc5ODoxQGxpZCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDT2phaHAwREVQSHA1TUlHR0FFZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiZ1ZKaEprWmFaTXpxODByRzM2N1czQUZneFN6YTNSSjV6bEtmRXl6WDVWYz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiSnJabFBicjd1MzRXcjFJTms5cHdwUWFFQ0c3azNqb1hLNDNzOWNDckRTWVVHV2VHS05JSU1teGt6eUJSSUNsbzMrVTRYaVgrZ0VBRkIyZFJBODFSQ2c9PSIsImRldmljZVNpZ25hdHVyZSI6IjUvcHF2NlFQMDlheEVaNUp1STBEQVpvRERFYVV5KzdSVy83cURtYUdSZmtKV054TEVZQzlFSDJxQjQya2c2UHZBcXNNRGZpZUY4RXpvcncrcHdSYURBPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjU0NzQ4OTczMDAzOjFAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWUZTWVNaR1dtVE02dk5LeHQrdTF0d0JZTVVzMnQwU2VjNVNueE1zMStWWCJ9fV0sInBsYXRmb3JtIjoic21iYSIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0FJSUNBPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzUwNjc2NzM2LCJsYXN0UHJvcEhhc2giOiIxSzRoSDQiLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUZUYiJ9",
         "required": true        
     },
       "PREFIX": {
@@ -31,7 +31,7 @@
     "ALWAYS_ONLINE": {
       "description": "Make it true if want always online.",
       "required": false,
-      "value": "false"  
+      "value": "true"  
    },      
     "AUTO_VOICE": {
       "description": "Make it true if want automatic voice reply .",
@@ -50,12 +50,12 @@
    }, 
     "AUTO_STATUS_REPLY": {
       "description": "Make it true for auto reply msg on status seen.",
-      "required": true,
+      "required": false,
       "value": "false"
    }, 
     "AUTO_STATUS_MSG": {
       "description": "Type custom message on status reply",
-      "required": true,
+      "required": false,
       "value": "false"   
    },     
     "OWNER_NAME": {
@@ -100,13 +100,13 @@
     },
     "AUTO_REACT": {
       "description": "Make it true if you want react on every message.",
-      "required": false,
-      "value": "false"
+      "required": true,
+      "value": "true"
     },    
     "CUSTOM_REACT": {
       "description": "Make it true if you want custom reactions.",
-      "required": false,
-      "value": "false"      
+      "required": true,
+      "value": "true"      
     },    
     "CUSTOM_REACT_EMOJIS": {
       "description": "put here custom react react emojis.",
